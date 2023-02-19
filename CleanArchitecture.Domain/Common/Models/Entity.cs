542b89ae9ecbe83dbd6bf545567399e639779c48
Frederik Bruun <frederikabruun@gmail.com>
2023-02-19 16:11:54 +0100

    Feature: Made into a template and added Commons with abstraction for entity and value object

---
+++ CleanArchitecture.Domain/Common/Models/Entity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace CleanArchitecture.Domain.Common.Models;
+
+public abstract class Entity<TEntityId>
+    where TEntityId : notnull
+{
+    private TEntityId Id { get; set; }
+
+    protected Entity(TEntityId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TEntityId> entity && Id.Equals(entity.Id);
+    }
+
+    public static bool operator ==(Entity<TEntityId> left, Entity<TEntityId> right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Entity<TEntityId> left, Entity<TEntityId> right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
